7073a4732e8c52c53288abed1a0949ea63b41aee
Zhenua1256 <56174998+Zhenua1256@users.noreply.github.com>
2019-10-16 00:02:51 +0300

    create TestController.java

---
+++ fapi/src/main/java/com/netcraker/devschool/fapi/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.netcraker.devschool.fapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("api/fapi")
+public class TestController {
+    @GetMapping
+    public HashMap<String, String> getTest() {
+
+        return new HashMap<>();
+    }
+
+}
