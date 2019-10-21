c1a9f00bda63a3b325dd9d82979a29e6a0d456db
Zhenua1256 <56174998+Zhenua1256@users.noreply.github.com>
2019-10-22 00:42:54 +0300

    fix

---
+++ backend/src/main/java/com/netcracker/dragun/NCApplication.java
@@ -0,0 +1,13 @@
+package com.netcracker.dragun;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NCApplication.class, args);
+    }
+
+}
---
+++ backend/src/main/resources/db.changelog/changes/cleanMD5.sql
@@ -0,0 +1,2 @@
+UPDATE databasechangelog
+SET md5sum=null
