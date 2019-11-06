cc42a1af005c481eb3618c8ac5c99b7f55cf2f67
Zhenua1256 <56174998+Zhenua1256@users.noreply.github.com>
2019-11-06 13:46:26 +0300

    create User dto

---
+++ backend/src/main/java/com/netcracker/dragun/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.netcracker.dragun.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Data
+@Builder
+@ToString
+public class UserDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String lastName;
+    private String login;
+    private String password;
+    private String role;
+}
