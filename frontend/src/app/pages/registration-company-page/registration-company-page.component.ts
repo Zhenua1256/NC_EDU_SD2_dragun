722d720afdc913c39a328952aafcd2f72df032b7
Zhenua1256 <56174998+Zhenua1256@users.noreply.github.com>
2019-11-11 20:33:54 +0300

    add registation

---
+++ frontend/src/app/pages/registration-company-page/registration-company-page.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'registration-company-page',
+  styleUrls: ['./registration-company-page.component.css'],
+  templateUrl: './registration-company-page.component.html'
+})
+export class RegistrationCompanyPageComponent {
+
+}
