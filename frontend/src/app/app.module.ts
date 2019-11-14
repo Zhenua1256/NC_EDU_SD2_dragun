0c5509de88af64863b7217577827e3c80cdb7978
Zhenua1256 <56174998+Zhenua1256@users.noreply.github.com>
2019-11-14 23:20:44 +0300

    fix

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,39 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BillingAccountComponent } from './components/billing-account/billing-account.component';
+/*import { BillingAccountComponent } from './components/billing-account/table/billing-account.component';*/
 import {UserModule} from './components/users/users.module';
 import {PagesModule} from './pages/pages.module';
 import {CompanyModule} from './components/company/company.module';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePageComponent} from './pages/home-page/home-page.component';
-import {RegistrationCompanyPageComponent} from './pages/registration-company-page/registration-company-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import {RegistrationUserPageComponent} from './pages/registration-user-page/registration-user-page.component';
-import { RegistrationComponent } from './registration/registration.component';
+import {RegistrationPageComponent} from './pages/registration-page/registration-page.component';
+import {CreateBillingAccountComponent} from './pages/create-billing-account-page/create-billing-account.component';
+import {RegistrationModule} from './registration/registration.module';
+/*
+import { RegistrationServicePageComponent } from './pages/registration-service-page/registration-service-page.component';
+import {RegistrationServiceComponent} from './components/registration-service/registration-service.component';*/
 
 const routes: Routes = [
   {path: '',  redirectTo: '/home', pathMatch: 'full'},
   {path: 'home' , component: HomePageComponent},
-  {path: 'registration', component: RegistrationComponent},
-  {path: 'registration/company', component: RegistrationCompanyPageComponent},
-  {path: 'registration/user', component: RegistrationUserPageComponent},
+  {path: 'registration', component: RegistrationPageComponent},
+  /*{path: 'registration_service', component: RegistrationServiceComponent},*/
+  {path: 'create_ba', component: CreateBillingAccountComponent},
   {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    BillingAccountComponent,
+   /* BillingAccountComponent,*/
     NotFoundComponent,
-    RegistrationComponent
+    RegistrationPageComponent
+  /*  RegistrationServicePageComponent*/
   ],
   imports: [
     UserModule,
     CompanyModule,
     BrowserModule,
     PagesModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    RegistrationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
