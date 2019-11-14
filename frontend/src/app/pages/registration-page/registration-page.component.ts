5fbaee45c5c84104a61b069344310759cfac0947
Zhenua1256 <56174998+Zhenua1256@users.noreply.github.com>
2019-11-14 23:20:05 +0300

    add registration component

---
+++ frontend/src/app/pages/registration-page/registration-page.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'registration-page',
+  styleUrls: ['./registration-page.component.css'],
+  templateUrl: './registration-page.component.html'
+})
+export class RegistrationPageComponent {
+
+}
--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {UserModel} from '../components/users/model/user.model';
+import {UserService} from '../service/User-Service';
+import {CompanyService} from '../service/Company-Service';
+import {CompanyModel} from '../components/company/model/company.model';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  users: UserModel[] = [];
+  company: CompanyModel[] = [];
 
-  ngOnInit() {
+  public currentUser: UserModel = {};
+  public  currentCompany: CompanyModel = {};
+  constructor(private userService: UserService, private companyService: CompanyService) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  public addUser(user?: UserModel): void {
+
+    console.log(this.currentUser);
+    this.userService.addUser(this.currentUser)
+      .subscribe(savedUser => {
+        this.users.push(savedUser);
+      });
+
+  }
+  public addCompany(company?: CompanyModel): void {
+
+    console.log(this.currentCompany);
+    this.companyService.addCompany(this.currentCompany)
+      .subscribe(savedCompany => {
+        this.company.push(savedCompany);
+      });
+
   }
 
 }
