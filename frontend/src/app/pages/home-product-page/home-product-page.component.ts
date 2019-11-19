7dedb32b626393bcc09f2cb5fbc05beefe858142
Zhenua1256 <56174998+Zhenua1256@users.noreply.github.com>
2019-11-19 21:58:39 +0300

    add home product page

---
+++ frontend/src/app/pages/home-product-page/home-product-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-product-page',
+  templateUrl: './home-product-page.component.html',
+  styleUrls: ['./home-product-page.component.css']
+})
+export class HomeProductPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
