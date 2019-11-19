0a66fb3c083065d3c42072501a0adb81245dce17
Zhenua1256 <56174998+Zhenua1256@users.noreply.github.com>
2019-11-19 21:57:19 +0300

    add registration

---
+++ frontend/src/app/components/registration/registration.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {UserModel} from '../users/model/user.model';
+import {UserService} from '../../service/User-Service';
+import {CompanyService} from '../../service/Company-Service';
+import {CompanyModel} from '../company/model/company.model';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  users: UserModel[] = [];
+  company: CompanyModel[] = [];
+
+  public currentUser: UserModel = {};
+  public currentCompany: CompanyModel = {};
+  constructor(private userService: UserService, private companyService: CompanyService) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  public addUser(user?: UserModel): void {
+
+    console.log(this.currentUser);
+    this.userService.addUser(this.currentUser)
+      .subscribe(savedUser => {
+        this.users.push(savedUser);
+      });
+
+  }
+  public addCompany(company?: CompanyModel): void {
+
+    console.log(this.currentCompany);
+    this.companyService.addCompany(this.currentCompany)
+      .subscribe(savedCompany => {
+        this.company.push(savedCompany);
+      });
+
+  }
+
+}
