9f49e940be1d40b7087a803d4b60703fa2291e0f
Zhenua1256 <56174998+Zhenua1256@users.noreply.github.com>
2019-11-29 20:58:45 +0300

    add personal area

---
+++ frontend/src/app/pages/personal-user-page/personal-user-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-user-page',
+  templateUrl: './personal-user-page.component.html',
+  styleUrls: ['./personal-user-page.component.css']
+})
+export class PersonalUserPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
