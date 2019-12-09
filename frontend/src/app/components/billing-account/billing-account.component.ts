4a53ed989e33a46b003e7656e5d711873c95a230
Zhenua1256 <56174998+Zhenua1256@users.noreply.github.com>
2019-12-09 20:24:28 +0300

    add load wallets

--- frontend/src/app/components/billing-account/billing-account.component.ts
+++ frontend/src/app/components/billing-account/billing-account.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {BillingAccountService} from '../../service/Billing-Account-Service';
 import {BillingAccountModel} from './model/billing-account.model';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {UserModel} from '../users/model/user.model';
 import {Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-billing-account',
@@ -15,8 +16,6 @@ export class BillingAccountComponent implements OnInit {
   newBillingAccountForm: FormGroup;
   newBillingAccount: BillingAccountModel = new BillingAccountModel();
   public currentUser: UserModel = {};
-  public billingAccounts: BillingAccountModel[];
-/*  private subscriptions: Subscription[] = [];*/
 
   constructor(private billingAccountService: BillingAccountService,
               private  formbuilder: FormBuilder,
@@ -29,7 +28,6 @@ export class BillingAccountComponent implements OnInit {
       cvv: ['', [Validators.required]],
     });
     this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
-  /*  this.loadBillingAccounts();*/
   }
 
   send() {
@@ -40,14 +38,5 @@ export class BillingAccountComponent implements OnInit {
     this.newBillingAccount.balance = 0;
     this.billingAccountService.saveBillingAccount(this.newBillingAccount).subscribe();
     this.router.navigate(["/personal"]);
-  }
-  private loadBillingAccounts(): void {
-    // Get data from BillingAccountService
-    /*this.subscriptions.push(this.billingAccountService.getBillingAccounts().subscribe(accounts => {
-      // Parse json response into local array
-      this.billingAccounts = accounts as BillingAccountModel[];
-      // Check data in console
-      this.loadingService.hide();
-    }));*/
   }
 }
--- frontend/src/app/components/product/details/details.component.ts
+++ frontend/src/app/components/product/details/details.component.ts
@@ -3,6 +3,9 @@ import {ProductService} from '../../../service/Product-service';
 import {ProductModel} from '../model/product.model';
 import {ActivatedRoute, Router} from '@angular/router';
 import {UserModel} from '../../users/model/user.model';
+import {BillingAccountModel} from '../../billing-account/model/billing-account.model';
+import {Subscription} from 'rxjs';
+import {BillingAccountService} from '../../../service/Billing-Account-Service';
 
 @Component({
   selector: 'app-details',
@@ -13,15 +16,20 @@ export class DetailsComponent implements OnInit {
   product: ProductModel = new ProductModel();
   productId: string;
   currentUser: UserModel;
+  private subscriptions: Subscription[] = [];
+  public billingAccounts: BillingAccountModel[] = [];
+
   period: string = "100";
   constructor(private productService: ProductService,
-              private activateRoute: ActivatedRoute) {
+              private activateRoute: ActivatedRoute,
+              private billingAccountService: BillingAccountService ) {
   }
 
   ngOnInit() {
     this.productId = this.activateRoute.snapshot.params["id"];
     this.getProduct(this.productId);
     this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    this.loadBillingAccounts();
   }
 
   public getProduct(id: string) {
@@ -33,5 +41,10 @@ export class DetailsComponent implements OnInit {
     this.productService.createSubcription(this.currentUser.id, this.productId, this.period).subscribe(model => {
       console.log(model);
     });
+  }
+  private loadBillingAccounts(): void {
+    this.subscriptions.push(this.billingAccountService.getBillingAccountUser(this.currentUser.id).subscribe(billingaccount => {
+      this.billingAccounts = billingaccount as BillingAccountModel[];
+    }));
   }
 }
